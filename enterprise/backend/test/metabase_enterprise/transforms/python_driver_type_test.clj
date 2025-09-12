0547f5d2eea473a0f26735aaf9025895cc64b1f8
Nicola Mometto <nicola.mometto@metabase.com>
2025-09-12 16:46:06 +0100

    bigq needs schema

--- enterprise/backend/test/metabase_enterprise/transforms/python_driver_type_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms/python_driver_type_test.clj
@@ -60,7 +60,7 @@
 (defn- execute-e2e-transform!
   "Execute an e2e Python transform test using execute-python-transform!"
   [table-name transform-code source-tables]
-  (let [schema (when (= driver/*driver* :postgres)
+  (let [schema (when (#{:postgres :bigquery-cloud-sdk} driver/*driver*)
                  (sql.tx/session-schema driver/*driver*))
         target {:type "table"
                 :schema schema
